be4fed4b6bd57eb3ceca2fb97fa5052acb785abc
Jason Livesay <ithkuil@gmail.com>
2014-04-11 01:15:46 -0700

    update

--- client.sh
+++ client.sh
@@ -8,21 +8,21 @@
 # Check to see that this is a command that applies to this plugin.
 # Sets default config values, installs programs appropriate
 # for this platform, etc.
-function oic-template_install()
+function plugin-template_install()
 {
-  if [[ "$1" == "oic-template" ]]; then
-    echo "Now installing oic-template plugin.."
+  if [[ "$1" == "plugin-template" ]]; then
+    echo "Now installing plugin-template plugin.."
   fi
 }
 
 # Single line command summary
-function oic-template_help_line()
+function plugin-template_help_line()
 {
   echo -e "  \e[4mmycmd \e[0m                                           \e[4mJust an example (does nothing)\e[0m"
 }
 
 # The rest of the command explanation. Be sure to include examples
-function oic-template_help_more()
+function plugin-template_help_more()
 {
   echo -e "    This is where we give a brief explanation and"
   echo -e "    then show some good examples:" 
@@ -33,10 +33,10 @@ function oic-template_help_more()
 # This will be executed every time an oic command is run.
 # Check to see if it is a command that this plugin handles
 # and run if it matches.
-function oic-template_run()
+function plugin-template_run()
 {
   if [[ "$1" == "mycmd" ]]; then
-    echo "Now running mycmd defined by oic-template."
+    echo "Now running mycmd defined by plugin-template."
     echo "Done."
   fi
 }
@@ -46,7 +46,7 @@ function oic-template_run()
 # This is an example that uses ANSI to
 # reverse the background/foreground
 # colors for the output of all commands.
-function oic-template_filter()
+function plugin-template_filter()
 {
   read output
   echo -e "\e[7m$output"
--- plugin.sh
+++ plugin.sh
@@ -1,4 +1,4 @@
-name=template
+name=plugin-template
 version=0.5
 description=oic plugin template
 url=https://github.com/ithkuil/oic-plugin-template.git
